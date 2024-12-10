63a842180b37756f252c2068cf82fb581f2b19cd
likitha <likkilikitha214@gmail.com>
2024-12-10 12:19:08 +0530

    Create add two numbers.java

---
+++ add two numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+		    int A=sc.nextInt();
+		    int B=sc.nextInt();
+		    System.out.println(A+B);
+		}
+}
+}
+		
