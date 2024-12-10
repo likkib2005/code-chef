ddae1359a1e555ca745540ea8c23bbd7fdb27e34
likitha <likkilikitha214@gmail.com>
2024-12-10 12:22:09 +0530

    Create chef and parole.java

---
+++ chef and parole.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    if(X>=7)
+	    {
+	        System.out.println("YES");
+	    }
+        else if(X>=30){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("NO");
+        }
+
+	}
+}
