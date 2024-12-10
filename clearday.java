5478855959c971fa90a4d244c8666c8aa2b1cf9a
likitha <likkilikitha214@gmail.com>
2024-12-10 14:00:52 +0530

    Create clearday.java

---
+++ clearday.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(X+Y==7)
+		{
+		    System.out.println("0");
+		    
+		}
+		else
+		{
+		    System.out.println((7-(X+Y)));
+		}
+
+	}
+}
