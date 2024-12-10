24f603b2a5cd37973ff5500ec951c36499d23d12
likitha <likkilikitha214@gmail.com>
2024-12-10 12:12:47 +0530

    Create moneymaking.java

---
+++ moneymaking.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes her
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		System.out.println(X*5000+Y*9800);
+
+	}
+}
