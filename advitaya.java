7fafa848749ad8fa090fe00689565a844a9d4190
likitha <likkilikitha214@gmail.com>
2024-12-10 12:23:24 +0530

    Create advitaya.java

---
+++ advitaya.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		if(N<16)
+		{
+		    System.out.println("WAITING FOR ADVITIYA");
+		}
+        else if(N>=18){
+        System.out.println("ADVITIYA");
+	}
+	else{
+	    System.out.println("ADVITIYA");
+	}
+}
+}
