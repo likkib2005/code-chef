fd2df3eb4a8b8e2b49762627081ea46880dc2c70
likitha <likkilikitha214@gmail.com>
2024-12-10 12:17:45 +0530

    Create devouring donuts.java

---
+++ devouring donuts.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(X<=10)
+		{
+		System.out.println(X*Y);
+		}
+		else
+		{
+		System.out.println("Message");
+		}
+	}
+}
